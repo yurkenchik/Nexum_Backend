52e5a105d53c06219db2a4c22f1bb5933aa0b9fc
yurkenchik <yura.ilchshyn@gmail.com>
2025-02-23 13:33:03 +0200

    updated player dto

--- src/application/dto/poker-session/join-game-queue.dto.ts
+++
@@ -1,4 +0,0 @@
-
-export class JoinGameQueueDto {
-    readonly playerCount: number;
-}
--- src/application/dto/poker-session/player.dto.ts
+++ src/application/dto/poker-session/player.dto.ts
@@ -1,9 +1,11 @@
 import { PlayingCardDocument } from 'src/domain/common/entities/playing-card.entity';
+import { PokerRole } from 'src/presentation/enums/poker-role.enum';
 
 export class PlayerDto {
     public socketId: string;
     public name: string;
     public amount: number;
+    public role: PokerRole;
     public cards: PlayingCardDocument;
 
 }
