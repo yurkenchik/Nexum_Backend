1b95fe8a9fda5344308abbc329662992dec740f4
yurkenchik <yura.ilchshyn@gmail.com>
2025-03-12 14:03:49 +0200

    Replaced strings in PlayingCard fields with enums

--- src/domain/common/entities/playing-card.entity.ts
+++ src/domain/common/entities/playing-card.entity.ts
@@ -1,15 +1,17 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { HydratedDocument } from "mongoose";
+import { PlayingCardSuits } from 'src/presentation/enums/playing-card-suits.enum';
+import { PlayingCardRank } from 'src/presentation/enums/playing-card-ranks.enum';
 
 export type PlayingCardDocument = HydratedDocument<PlayingCard>;
 
 @Schema({ timestamps: true })
 export class PlayingCard {
-    @Prop({ required: true })
-    public suit: string;
+    @Prop({ required: true, enum: PlayingCardSuits })
+    public suit: PlayingCardSuits;
 
-    @Prop({ required: true })
-    public rank: string;
+    @Prop({ required: true, enum: PlayingCardRank })
+    public rank: PlayingCardRank;
 
     @Prop({ required: true })
     public code: string;
