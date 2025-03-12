04fe229fbdf6f9b0f50d41570f5193686965044a
yurkenchik <yura.ilchshyn@gmail.com>
2025-03-12 14:08:39 +0200

    Replaced error handling with normal excpetions

--- src/infrastructure/user/user.service.ts
+++ src/infrastructure/user/user.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { CreateUserDto } from "src/application/dto/user/create-user.dto";
 import { User, UserDocument } from "src/domain/common/entities/user.entity";
+import { UserNotFoundException } from 'src/domain/common/exceptions/client/user-not-found.exception';
 
 @Injectable()
 export class UserService {
@@ -22,14 +23,12 @@ export class UserService {
     async getUserById(id: string): Promise<UserDocument> {
         const user = await this.userModel.findOne({ id });
         if (!user) {
-            throw new NotFoundException("User not found");
+            throw new UserNotFoundException();
         }
         return user;
     }
 
     async getUseByEmail(email: string): Promise<UserDocument | null> {
-        const user = await this.userModel.findOne({ email });
-        console.log("USER: ", user);
-        return user;
+        return this.userModel.findOne({ email });
     }
 }
